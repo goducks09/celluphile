bae15d6d09c943a03965b6d50c28906151bf41a0
Chris <33887464+goducks09@users.noreply.github.com>
2025-11-27 09:27:01 -0800

    Add auth and db connection

---
+++ auth.ts
@@ -0,0 +1,75 @@
+import NextAuth from 'next-auth';
+import { MongoDBAdapter } from '@auth/mongodb-adapter';
+import client from '@/app/lib/db';
+import Credentials from 'next-auth/providers/credentials';
+import User from '@/app/models/user';
+import dbConnect from '@/app/lib/db';
+import bcrypt from 'bcryptjs';
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: MongoDBAdapter(client),
+  session: { strategy: 'jwt' },
+  providers: [
+    Credentials({
+      name: 'Credentials',
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials.password) {
+            return null;
+        }
+
+        await dbConnect();
+
+        const user = await User.findOne({ email: credentials.email });
+
+        if (!user || !user.password) {
+          return null;
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(
+          credentials.password as string,
+          user.password
+        );
+
+        if (isPasswordCorrect) {
+          // Return user object without the password
+          return { id: user._id.toString(), email: user.email, name: user.name };
+        }
+
+        return null;
+      },
+    }),
+  ],
+  pages: {
+    signIn: '/login',
+  },
+  callbacks: {
+    // This callback is used to customize the JWT token
+    async jwt({ token, user }) {
+        if (user) {
+            token.id = user.id;
+        }
+        return token;
+    },
+    // This callback is used to customize the session object
+    async session({ session, token }) {
+        if (session.user) {
+            session.user.id = token.id as string;
+        }
+        return session;
+    },
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      if (isOnDashboard) {
+        if (isLoggedIn) return true;
+        return false; // Redirect unauthenticated users to login page
+      } else if (isLoggedIn) {
+        // Redirect logged-in users from login/register pages to the dashboard
+        if (nextUrl.pathname.startsWith('/login') || nextUrl.pathname.startsWith('/register')) {
+          return Response.redirect(new URL('/dashboard', nextUrl));
+        }
+      }
+      return true;
+    },
+  },
+});
