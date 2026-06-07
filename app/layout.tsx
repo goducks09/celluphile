b7d14c824c1e41cb13ce530af223ff60f3ebde94
Chris <chris@example.com>
2026-06-07 14:16:40 -0700

    Add PWA splash screen

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,74 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Celluphile Movie Library",
   description: "The personal movie library app for movie lovers.",
+  themeColor: '#1a1a2e',
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: 'black-translucent',
+    title: 'Celluphile',
+    startupImage: [
+      // iPhone SE 3rd, 8
+      {
+        url: '/splash/splash-750x1334.png',
+        media: '(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)',
+      },
+      // iPhone 8 Plus
+      {
+        url: '/splash/splash-1242x2208.png',
+        media: '(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3)',
+      },
+      // iPhone X, XS, 11 Pro
+      {
+        url: '/splash/splash-1125x2436.png',
+        media: '(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)',
+      },
+      // iPhone XR, 11
+      {
+        url: '/splash/splash-828x1792.png',
+        media: '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)',
+      },
+      // iPhone XS Max, 11 Pro Max
+      {
+        url: '/splash/splash-1242x2688.png',
+        media: '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)',
+      },
+      // iPhone 12 mini, 13 mini
+      {
+        url: '/splash/splash-1080x2340.png',
+        media: '(device-width: 360px) and (device-height: 780px) and (-webkit-device-pixel-ratio: 3)',
+      },
+      // iPhone 12, 13, 14
+      {
+        url: '/splash/splash-1170x2532.png',
+        media: '(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3)',
+      },
+      // iPhone 12/13 Pro Max, 14 Plus
+      {
+        url: '/splash/splash-1284x2778.png',
+        media: '(device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3)',
+      },
+      // iPhone 14 Pro, 15, 15 Pro, 16
+      {
+        url: '/splash/splash-1179x2556.png',
+        media: '(device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3)',
+      },
+      // iPhone 14 Pro Max, 15 Plus, 15 Pro Max, 16 Plus
+      {
+        url: '/splash/splash-1290x2796.png',
+        media: '(device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3)',
+      },
+      // iPhone 16 Pro
+      {
+        url: '/splash/splash-1206x2622.png',
+        media: '(device-width: 402px) and (device-height: 874px) and (-webkit-device-pixel-ratio: 3)',
+      },
+      // iPhone 16 Pro Max
+      {
+        url: '/splash/splash-1320x2868.png',
+        media: '(device-width: 440px) and (device-height: 956px) and (-webkit-device-pixel-ratio: 3)',
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
